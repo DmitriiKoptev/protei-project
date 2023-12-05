282f74159a20784476a46839e880bbf62ee7f7c0
DmitriiKoptev <112893017+DmitriiKoptev@users.noreply.github.com>
2023-12-06 00:48:34 +0300

    add packages directory with modules

---
+++ packages/module-hello-world/Hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../src/client/assets/styles/style.css'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const Hello = () => {
+  return (
+        <>
+            <h1>Hello World!</h1>
+        </>
+  )
+}
+
+export default Hello
